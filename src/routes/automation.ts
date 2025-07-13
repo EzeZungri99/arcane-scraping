d961a7619d2071f10ed338966cf934312f3c115b
Ezequiel Zungri <zungri2020@gmail.com>
2025-07-13 01:45:42 -0300

    Add new functionality to the automation controller, including methods for filtering, downloading, unlocking, and extracting century codes. The Playwright service is added to handle these operations. Routes are also updated to reflect these changes.

--- src/routes/automation.ts
+++ src/routes/automation.ts
@@ -9,9 +9,9 @@ router.get('/status', (req, res) => automationController.getStatus(req, res));
 router.get('/browser-status', (req, res) => automationController.getBrowserStatus(req, res));
 router.post('/filter/:century', (req, res) => automationController.filterByCentury(req, res));
 router.post('/download/:century', (req, res) => automationController.downloadPDF(req, res));
-router.post('/unlock/:century', (req, res) => automationController.unlockCentury(req, res));
-router.post('/extract-code/:century', (req, res) => automationController.extractCodeFromPDF(req, res));
+router.post('/unlock', (req, res) => automationController.unlockCentury(req, res));
+router.post('/extract/:century', (req, res) => automationController.extractCodeFromPDF(req, res));
 router.post('/process-century/:century', (req, res) => automationController.processSpecificCentury(req, res));
-router.post('/run-full-automation', (req, res) => automationController.runCompleteFlow(req, res));
+router.post('/start', (req, res) => automationController.runCompleteFlow(req, res));
 
 export { router as automationRoutes }; 
