08c47e0a8d3b8b68ebdc92ec896fc9034e8e2e93
Ezequiel Zungri <zungri2020@gmail.com>
2025-07-13 00:59:59 -0300

    Add new automation routes for login, filtering, downloading, unlocking, code extraction, and century processing in the automation.ts file.

--- src/routes/automation.ts
+++ src/routes/automation.ts
@@ -4,8 +4,14 @@ import { AutomationController } from '../controllers/automation';
 const router: Router = Router();
 const automationController = new AutomationController();
 
-router.post('/start', (req, res) => automationController.startAutomation(req, res));
+router.post('/login', (req, res) => automationController.startAutomation(req, res));
 router.get('/status', (req, res) => automationController.getStatus(req, res));
 router.get('/browser-status', (req, res) => automationController.getBrowserStatus(req, res));
+router.post('/filter/:century', (req, res) => automationController.filterByCentury(req, res));
+router.post('/download/:century', (req, res) => automationController.downloadPDF(req, res));
+router.post('/unlock/:century', (req, res) => automationController.unlockCentury(req, res));
+router.post('/extract-code/:century', (req, res) => automationController.extractCodeFromPDF(req, res));
+router.post('/process-century/:century', (req, res) => automationController.processSpecificCentury(req, res));
+router.post('/run-full-automation', (req, res) => automationController.runCompleteFlow(req, res));
 
 export { router as automationRoutes }; 
